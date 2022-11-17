f9b6f299259661f085d5cab451837cffc3fa6338
Shaheer Sarfaraz <shaheergoodboy@gmail.com>
2022-11-17 11:59:08 +0000

    Logical errors fixed, reworked functions to avoid refreshing page

--- cartJs.js
+++ cartJs.js
@@ -1,66 +1,85 @@
-// get and show cart from session storage
-var cart = JSON.parse(localStorage.getItem("cart"));
+function generateCartItems() {
+    // empty the list to add updated items everytime
+    let cart = JSON.parse(localStorage.getItem("cart"));
 
-console.log(cart);
+    // if cart is empty, show empty cart message
+    if (cart.length == 0) {
+        // disable checkout button
+        document.getElementById("checkoutButton").disabled = true;
 
-// for every item in cart, create a card
-for (var i = 0; i < cart.length; i++) {
-    var product = cart[i];
-    var productString = JSON.stringify(product);
+        // remove the remove all button
+        document.getElementsByClassName("header")[0].style.display = "none";
 
-    document.getElementById("cartItemsList").innerHTML += `
-    <div class="cartItem">
-        <div class="image-box">
-            <img
-                src="${cart[i][4]}"
-                alt="${cart[i][2]}"
-            />
+        // show empty cart message
+        document.getElementById("cartItemsList").innerHTML = `
+        <div class="empty-cart">
+            <h1 class="title">Your cart is empty</h1>
+            <h3 class="subtitle">Add some items to your cart</h3>
         </div>
-        <div class="about">
-            <h1 class="title">${cart[i][0]}</h1>
-            <h3 class="subtitle">${cart[i][1]}</h3>
-        </div>
-        <div class="prices">
-            <div class="amount">${cart[i][3]}</div>
-            <div class="remove" onclick='removeItem(${productString})'>
-                Remove
+`;
+
+        // end execution of function
+        return;
+    }
+
+    document.getElementById("cartItemsList").innerHTML = "";
+
+    // for every item in cart, create a card
+    for (let i = 0; i < cart.length; i++) {
+        let product = cart[i];
+        let productString = JSON.stringify(product);
+
+        document.getElementById("cartItemsList").innerHTML += `
+        <div class="cartItem">
+            <div class="image-box">
+                <img
+                    src="${cart[i][4]}"
+                    alt="${cart[i][2]}"
+                />
+            </div>
+            <div class="about">
+                <h1 class="title">${cart[i][0]}</h1>
+                <h3 class="subtitle">${cart[i][1]}</h3>
+            </div>
+            <div class="prices">
+                <div class="amount">${cart[i][3]}</div>
+                <div class="remove" onclick='removeItem(${productString})'>
+                    Remove
+                </div>
             </div>
         </div>
-    </div>
-    `;
+        `;
+    }
 }
 
-// if cart is empty, show empty cart message
-if (cart.length == 0) {
-
-    // disable checkout button
-    document.getElementById("checkoutButton").disabled = true;
-
-    // remove the remove all button
-    document.getElementsByClassName("header")[0].style.display = "none";
-
-    // show empty cart message
-    document.getElementById("cartItemsList").innerHTML = `
-    <div class="empty-cart">
-        <h1 class="title">Your cart is empty</h1>
-        <h3 class="subtitle">Add some items to your cart</h3>
-    </div>
-    `;
-}
+// generate cart items on page load
+generateCartItems();
 
 // remove all items from cart
 function removeAllItems() {
     localStorage.setItem("cart", "[]");
-    location.reload();
+    generateCartItems();
 }
 
 // removes item from cart
 function removeItem(item) {
-    var cart = JSON.parse(localStorage.getItem("cart"));
-    var index = cart.indexOf(item);
-    cart.splice(index, 1);
+    // https://stackoverflow.com/questions/5767325/how-can-i-remove-a-specific-item-from-an-array
+    let cart = JSON.parse(localStorage.getItem("cart"));
+
+    // check if item is in cart and remove it
+    // loop through cart
+    for (let i = 0; i < cart.length; i++) {
+        // if item is in cart, image urls are the same
+        if (cart[i][4] == item[4]) {
+            // remove item from cart
+            cart.splice(i, 1);
+        }
+    }
+
     localStorage.setItem("cart", JSON.stringify(cart));
-    location.reload();
+
+    generateCartItems();
+    calculateTotal();
 }
 
 // saves the item to session storage
@@ -73,19 +92,28 @@ function saveItem(item) {
 
 // calculates the total price of the cart
 function calculateTotal() {
-    var total = 0;
-    for (var i = 0; i < cart.length; i++) {
-        // https://stackoverflow.com/questions/30607419/return-only-numbers-from-string
-        var price = cart[i][3].replace(/[^0-9.-]+/g, "");
-        total += parseFloat(price);
+    let cart = JSON.parse(localStorage.getItem("cart"));
+    let total = 0;
+
+    // if cart is empty, show empty cart message
+    if (cart.length == 0) {
+        document.getElementById("total").innerHTML = `
+        <div class="total"></div>
+`;
+
+        for (let i = 0; i < cart.length; i++) {
+            // https://stackoverflow.com/questions/30607419/return-only-numbers-from-string
+            let price = cart[i][3].replace(/[^0-9.-]+/g, "");
+            total += parseFloat(price);
+        }
+
+        // round to 2 decimals
+        total = total.toFixed(2);
+
+        // show total price
+        // adds the total price to the cart
+        document.getElementById("totalPrice").innerHTML = `£${total}`;
+        // set the total number of items in the cart
+        document.getElementById("numItems").innerHTML = `${cart.length} items`;
     }
-
-    // round to 2 decimals
-    total = total.toFixed(2);
-    return total;
 }
-
-// adds the total price to the cart
-document.getElementById("totalPrice").innerHTML = `£${calculateTotal()}`;
-// set the total number of items in the cart
-document.getElementById("numItems").innerHTML = `${cart.length} items`;
