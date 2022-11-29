fc10751722bcaef7d3f12b5e273e46c538a74e81
Shaheer Sarfaraz <shaheergoodboy@gmail.com>
2022-11-29 09:32:05 +0000

    If no item is in sessionStorage, display error

--- itemJs.js
+++ itemJs.js
@@ -1,16 +1,28 @@
 // get item from session storage
 var product = JSON.parse(sessionStorage.getItem("product"));
 
-// set item to html
-document.getElementById("displayItem").innerHTML = `
-<div class="card">
-    <img
-        src="${product[4]}"
-        alt="${product[2]}"
-    />
-    <h3>${product[0]}</h3>
-    <p class="color">${product[1]}</p>
-    <p class="price">${product[3]}</p>
-    <p>${product[2]}</p>
-</div>
-`;
+if (product != null) {
+    // set item to html
+    document.getElementById("displayItem").innerHTML = `
+    <div class="card">
+        <img
+            src="${product[4]}"
+            alt="${product[2]}"
+        />
+        <h3>${product[0]}</h3>
+        <p class="color">${product[1]}</p>
+        <p class="price">${product[3]}</p>
+        <p>${product[2]}</p>
+    </div>
+    `;
+} else {
+    // if no item is found, show error message
+    document.getElementById("displayItem").innerHTML = `
+    <div class="error">
+        <h1 class="title">Error</h1>
+        <h3 class="subtitle">
+            No item found. Please go through the Product page to view an item.
+        </h3>
+    </div>
+    `;
+}
