b0cdbc9b23660eb9f1ca630e40569803a72e3bf2
Shaheer Sarfaraz <shaheergoodboy@gmail.com>
2022-11-29 10:19:59 +0000

    Updating generated item html to most recent styles

--- itemJs.js
+++ itemJs.js
@@ -5,16 +5,18 @@ if (product != null) {
     // set item to html
     document.getElementById("displayItem").innerHTML = `
     <div class="card">
-        <img
-            src="${product[4]}"
-            alt="${product[2]}"
-        />
-        <h3>${product[0]}</h3>
-        <p class="color">${product[1]}</p>
-        <p class="price">${product[3]}</p>
-        <p>${product[2]}</p>
+        <div class="cardDetails">
+            <img
+                src="${product[4]}"
+                alt="${product[2]}"
+            />
+            <h3>${product[0]}</h3>
+            <p class="color">${product[1]}</p>
+            <p class="price">${product[3]}</p>
+            <p>${product[2]}</p>
+        </div>
     </div>
-    `;
+`;
 } else {
     // if no item is found, show error message
     document.getElementById("displayItem").innerHTML = `
